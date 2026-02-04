5e03fcb08e0c810dfafeb03ec42127a1225f7678
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2026-02-04 07:53:20 +0600

    style(spinner): Remove extra newline from suffix message

--- utils/gemini.go
+++ utils/gemini.go
@@ -21,7 +21,7 @@ func GetMeaningfulCommitMessage(changes string) string {
 	client, err := genai.NewClient(ctx, &genai.ClientConfig{APIKey: apiKey})
 	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
 
-	s.Suffix = " Generating commit message...\n\n"
+	s.Suffix = " Generating commit message...\n"
 	s.Color("cyan")
 	s.HideCursor = true
 	s.Writer = os.Stderr
