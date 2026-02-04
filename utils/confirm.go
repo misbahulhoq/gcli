3cd9436eab8f13a2dc50232275ccd93a1c7133e3
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2026-02-04 07:54:25 +0600

    docs(confirm): Simplify comment regarding default 'yes' input

--- utils/confirm.go
+++ utils/confirm.go
@@ -15,7 +15,7 @@ func Confirm(message string) bool {
 	input, _ := reader.ReadString('\n')
 
 	input = strings.TrimSpace(strings.ToLower(input))
-	// We check for "y", "yes", or empty string (if you want Enter to mean Yes)
+	// We check for "y", "yes", or empty string (Enter to mean Yes)
 	if input == "y" || input == "yes" || input == "" {
 		return true
 	}
